6fec604569511a5ba86e62d04c9729157061bb1b
mindw <mindw96@naver.com>
2021-08-06 18:38:45 +0900

    Initial commit

---
+++ 2231.py
@@ -0,0 +1,10 @@
+N = int(input())
+sol = N
+
+for i in range(len(str(N)) - 1, -1, -1):
+    print(N // (10 ** i))
+    sol += N // (10 ** i)
+    N -= (N // (10 ** i)) * (10 ** i)
+    print(N)
+
+print(sol)
