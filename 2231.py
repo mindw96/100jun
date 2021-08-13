b5a0cfa673594f1d81eae1a724eafd8ac5ec5d8d
mindw <mindw96@naver.com>
2021-08-13 21:36:26 +0900

    add No.2231

--- 2231.py
+++ 2231.py
@@ -2,9 +2,9 @@ N = int(input())
 sol = N
 
 for i in range(len(str(N)) - 1, -1, -1):
-    print(N // (10 ** i))
+#    print(N // (10 ** i))
     sol += N // (10 ** i)
     N -= (N // (10 ** i)) * (10 ** i)
-    print(N)
+#    print(N)
 
 print(sol, N)
